2e9cf445647f8f5925a189732d04048c38733bfc
RNHTTR <ryan@wiftapp.com>
2018-05-03 10:27:18 -0400

    Better documentation and organization in neural network

--- app/2_prediction/DenseNeuralNetwork.py
+++ app/2_prediction/DenseNeuralNetwork.py
@@ -1,3 +1,4 @@
+import argparse
 import math
 import sys
 
@@ -12,10 +13,17 @@ from tensorflow.python.keras.layers import Dense, Dropout, BatchNormalization
 
 
 def compile(dim):
-    '''
-    Compile the keras neural network model
+    '''Compile the keras neural network model
 
-    Needs to take in **kwargs params later
+    Args:
+        dim (int): Input data dimension
+
+    Returns:
+        model (keras Sequential model): Compiled neural network model ready to
+                                        be trained
+
+    TODO:
+        Needs to take in **kwargs params later, e.g. kernel_initializer, activations
     '''
     model = Sequential()
 
@@ -35,20 +43,34 @@ def compile(dim):
     return model
 
 
-def model_train_data(input_file_name):
+def data_to_train(train_data_file_name):
+    '''Get the data to train the model
+
+    Args:
+        train_data_file_name (str): Name of file for training data
+
+    Returns:
+        X (pd.DataFrame): X_train
+        y (pd.Series)   : y_train
     '''
-    '''
-    df = pd.read_csv(input_file_name)
+    df = pd.read_csv(train_data_file_name)
 
     X = df.drop('dk_points', axis=1)
     y = df['dk_points']
     return X, y
 
 
-def model_predict_data(input_file_name, model_file_name, scaler=RobustScaler()):
+def predict_data(X_data_file_name, scaler=RobustScaler()):
+    '''Get the data to be used to generate predictions
+
+    Args:
+        X_data_file_name (str): Name of file for X data
+
+    Returns:
+        data (:obj:`dict` of :obj:obj): Contains the scaled X data, ids,
+                                        salaries, and position data
     '''
-    '''
-    X = pd.read_csv(input_file_name)
+    X = pd.read_csv(X_data_file_name)
 
     scaled_X = scaler.fit_transform(X)
 
@@ -56,13 +78,12 @@ def model_predict_data(input_file_name, model_file_name, scaler=RobustScaler()):
     salaries = X['dk_salary'].reset_index().drop('index', axis=1)
     pos      = X['fd_pos'].reset_index().drop('index', axis=1)
 
-    model = load_model(model_file_name)
-
-    return {'model'   : model,
-            'scaled_X': scaled_X,
+    data = {'scaled_X': scaled_X,
             'ids'     : ids,
             'salaries': salaries,
             'pos'     : pos}
+
+    return data
 
 
 def fit(X, y, should_save=False, model_file_name=None, seed=False, scaler=RobustScaler()):
@@ -70,7 +91,6 @@ def fit(X, y, should_save=False, model_file_name=None, seed=False, scaler=Robust
     Fit the keras neural network model
 
     Args:
-        # input_file_name (str)  : Input data file name
         X                      : input X data
         y                      : input y data
         scaler (sklearn scaler): sk learn feature scaling object
@@ -124,8 +144,16 @@ def fit(X, y, should_save=False, model_file_name=None, seed=False, scaler=Robust
 
 
 def evaluate(data, should_evaluate=True):
-    '''
+    '''Evaluate model performance and generate predictions
 
+    Args:
+        data (:obj:`dict` of :obj:obj): Contains the model, scaled X data, ids,
+                                        salaries, and position data
+        should_evaluate (bool)        : Whether or not to use the Keras evaluate
+                                        method
+
+    Returns:
+        predictions (numpy array)     : Generates output predictions
     '''
     scaled_X_test = data['scaled_X']
     model = data['model']
@@ -139,22 +167,80 @@ def evaluate(data, should_evaluate=True):
     return predictions
 
 
-def main(output_file_name, predict, **kwargs):
+def compare_projected_actual(predictions, y_test):
+    '''Prints accuracy details based on averages of projected and actual points
+    scored
+
+    Args:
+        predictions (numpy array): Actual output predictions
+        y_test      (numpy array): Test output predictions
+
+    TODO:
+        Calculate averages. 4.5 and 7.3 are the averages for predicted data and
+        actual DK points data.
     '''
+    i = 0
+    total = 0
+    total_y = 0
+    good = 0
+    bad = 0
+    for pred, y in zip(predictions, y_test):
+        i += 1
+        diff = abs(pred - y)
+        total += diff
+        total_y += y
+        if pred >= 4.5:
+            if y >= 7.3:
+                good += 1
+            else:
+                bad += 1
+        elif pred < 4.5:
+            if y < 7.3:
+                good += 1
+            else:
+                bad += 1
+
+    print('Predictions average: ', predictions.mean())
+    print('Test average: ', y_test.mean())
+    print('good!! {}'.format(good))
+    print('bad :( {}'.format(bad))
+    print('average difference: {}'.format(total / i))
+
+
+def main(output_file_name, predict, **kwargs):
+    '''Triggers the neural network processes based on various parameters.
+    Generates predictions and writes the predictions to a csv.
+
+    Args:
+        output_file_name (str): File name of generated predictions csv
+        predict (bool)        : True if making predictions on unseen data
+        kwargs:
+            x_file (str)            : File name of X data
+            model_file_name (str)   : File name of saved neural network model
+            training_file_name (str): File name of training data
+            should_save (bool)      : Whether the model should be saved
+
+            # NOTE: Some of these are mutually exclusive. For example, if
+                    should_save is True, you should not pass a training_file_name
+                    and vice versa.
+
     '''
     if predict:
         assert 'x_file' in kwargs and 'model_file_name' in kwargs, \
             'When making real predictions, (i.e. Predict = True), you must pass \
             an X_file and a model_file_name to load X data and the model.'
-        model_details = model_predict_data(kwargs['x_file'], kwargs['model_file_name'])
 
-        predictions = evaluate(model_details, should_evaluate=False)
+        model_details = predict_data(kwargs['x_file'])
+        model_details['model'] = load_model(kwargs['model_file_name'])
+
+        predictions   = evaluate(model_details, should_evaluate=False)
+
     else:
         assert 'training_file_name' in kwargs, \
             'A path to a training file must be given if you are not predicting \
-            results.'
+            results, e.g. batter_data.csv'
 
-        X, y = model_train_data(kwargs['training_file_name'])
+        X, y = data_to_train(kwargs['training_file_name'])
 
         if 'should_save' in kwargs:
             if kwargs['should_save']:
@@ -171,39 +257,13 @@ def main(output_file_name, predict, **kwargs):
 
         predictions = evaluate(model_details, should_evaluate=True)
 
-        # Calculate average difference between projected and actual points scored
-        i = 0
-        total = 0
-        total_y = 0
-        good = 0
-        bad = 0
-        for pred, y in zip(predictions, y_test):
-            i += 1
-            diff = abs(pred - y)
-            total += diff
-            total_y += y
-            if pred >= 4.5:
-                if y >= 7.3:
-                    good += 1
-                else:
-                    bad += 1
-            elif pred < 4.5:
-                if y < 7.3:
-                    good += 1
-                else:
-                    bad += 1
-
-        print(predictions.mean())
-        print(y_test.mean())
-        print('good!! {}'.format(good))
-        print('bad :( {}'.format(bad))
-        print('average difference: {}'.format(total / i))
+        compare_projected_actual(predictions, y_test)
 
     ids            = model_details['ids']
     salaries       = model_details['salaries']
     pos            = model_details['pos']
 
-    # Generate output predictions file for batters
+    # Reformat output predictions to write to csv
     predictions = predictions.tolist()
     predictions = pd.Series(predictions).rename("Predictions")
     predictions = predictions.apply(lambda x: x[0])
@@ -213,6 +273,9 @@ def main(output_file_name, predict, **kwargs):
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
+        # NOTE - The predict command line arg is misleading. Predictions are always generated.
+        #        Should be renamed to "train" or similar
+        # TODO - Reformat to config file
         required_parameter_keys = {'output_file_name', 'predict'}
         optional_parameter_keys = {'training_file_name', 'should_load',
                                    'model_file_name', 'should_save', 'X_file'}
