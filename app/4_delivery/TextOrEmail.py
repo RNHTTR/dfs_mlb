4ddfcd1dc3ef70d80082a61cfc095502faf047d7
RNHTTR <ryan@wiftapp.com>
2018-05-15 09:21:09 -0400

    blocking config.yaml so my phone number isn't exposed :)

--- app/4_delivery/TextOrEmail.py
+++ app/4_delivery/TextOrEmail.py
@@ -4,11 +4,11 @@ Placeholder file for delivering team results
 
 ##################### EMAIL #####################
 # Import smtplib for the actual sending function
-import smtplib
-
-# Here are the email package modules we'll need
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
+# import smtplib
+#
+# # Here are the email package modules we'll need
+# from email.mime.image import MIMEImage
+# from email.mime.multipart import MIMEMultipart
 
 # Do some stuff
 #################################################
@@ -24,7 +24,7 @@ auth_token  = "your_auth_token"
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+18135555555", 
+    to="+18135555555",
     from_="+15017250604",
     body="Hello from Python!")
 
