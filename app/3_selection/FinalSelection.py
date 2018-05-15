2c15fccdf5309eb97b0c4cdf6e29025922160cd9
RNHTTR <ryan@wiftapp.com>
2018-05-15 08:47:03 -0400

    Added commentary and more obvious variable names

--- app/3_selection/FinalSelection.py
+++ app/3_selection/FinalSelection.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-# NOTE: Needs to be merged into Knapsack.py
+
+# Read Knapsack.py output and organize into clean DataFrame
 df = pd.read_csv('selections.csv', index_col=0, sep='|')
 df = df.apply(lambda x: x.str.lstrip('(').str.rstrip(')').str.split(', '), axis=1)
 idx = ['C', '1B', '2B', 'SS', '3B', 'OF']
@@ -8,28 +9,35 @@ df['idx'] = idx
 df = df.set_index('idx')
 df.index.name = None
 
-final_selection = {}
-of = []
 
+infield = {}
+outfield = []
+
+# NOTE: Consider refactoring into well-named functions for easier readability
+# Select players to draft
 summ = 0
 for i in idx:
     for j in range(6):
+        # Get a list of four outfielders (one possibly for util position)
         if i == 'OF':
-            if len(of) < 3:
-                of.append(df.loc[i].iloc[j])
+            if len(outfield) < 3:
+                outfield.append(df.loc[i].iloc[j])
             else:
-                for k in range(len(of)):
-                    if df.loc[i].iloc[j][2] > of[k][2]:
-                        of[k] = df.loc[i].iloc[j]
+                # Replace players with players who have a higher projected score
+                for k in range(len(outfield)):
+                    if df.loc[i].iloc[j][2] > outfield[k][2]:
+                        outfield[k] = df.loc[i].iloc[j]
         else:
-            if i in final_selection:
-                if df.loc[i].iloc[j] > final_selection[i]:
-                    final_selection[i] = df.loc[i].iloc[j]
+            # Select players with highest projected score
+            if i in infield:
+                # Replace players with players who have a higher projected score
+                if df.loc[i].iloc[j] > infield[i]:
+                    infield[i] = df.loc[i].iloc[j]
             else:
-                final_selection[i] = df.loc[i].iloc[j]
+                infield[i] = df.loc[i].iloc[j]
 
 # Get Infielders
-df = pd.DataFrame.from_dict(final_selection, orient='index', dtype=None)
+df = pd.DataFrame.from_dict(infield, orient='index', dtype=None)
 df.columns = ['mlb_id', 'dk_salary', 'dk_points']
 df['mlb_id'] = pd.to_numeric(df['mlb_id'], downcast='integer')
 player_id_map_df = pd.read_csv('player_id_map.csv')
@@ -37,9 +45,9 @@ df = pd.merge(df, player_id_map_df, on='mlb_id', right_index=True)
 # print(player_id_map_df.head())
 
 # Get outfielders
-of = pd.DataFrame(of, columns=['mlb_id', 'dk_salary', 'dk_points'], index=['OF','OF','OF'])
-of['mlb_id'] = pd.to_numeric(of['mlb_id'], downcast='integer')
-of = pd.merge(of, player_id_map_df, on='mlb_id', right_index=True)
+outfield = pd.DataFrame(outfield, columns=['mlb_id', 'dk_salary', 'dk_points'], index=['OF','OF','OF'])
+outfield['mlb_id'] = pd.to_numeric(outfield['mlb_id'], downcast='integer')
+outfield = pd.merge(outfield, player_id_map_df, on='mlb_id', right_index=True)
 
-final_selection = pd.concat([df, of])
-print(final_selection)
+all_players = pd.concat([df, outfield])
+print(all_players)
